de8aa2be7ac224c99f89e039a1579ec86ef93537
babachoo <46634893+babachoo@users.noreply.github.com>
2019-01-12 17:38:24 -0500

    Add files via upload

--- Scripts/Gravegrump.cs
+++ Scripts/Gravegrump.cs
@@ -10,7 +10,7 @@ public class Gravegrump : Enemy
     public float attackRadius;
     public Transform homePosition;
     public Animator anim;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -41,7 +41,7 @@ public class Gravegrump : Enemy
 
             changeAnim(temp - transform.position);
             myRigidbody.MovePosition(temp);
-            
+
             ChangeState(EnemyState.walk);
             anim.SetBool("wakeUp", true);
         }
@@ -50,51 +50,50 @@ public class Gravegrump : Enemy
             anim.SetBool("wakeUp", false);
         }
     }
-}
 
 
 
-private void SetAnimFloat(Vector2 setVector)
-{
-    anim.SetFloat("moveX", setVector.x);
-    anim.SetFloat("moveY", setVector.y);
-}
 
-private void changeAnim(Vector2 direction)
-{
-    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+    private void SetAnimFloat(Vector2 setVector)
     {
-        if (direction.x > 0)
-        {
-            SetAnimFloat(Vector2.right);
-        }
-        else if (direction.x < 0)
-        {
-            SetAnimFloat(Vector2.left);
-        }
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
     }
 
-    else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+    private void changeAnim(Vector2 direction)
     {
-        if(direction.y > 0)
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
         {
-            SetAnimFloat(Vector2.up);
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if (direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
         }
-        else if (direction.y < 0)
+
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
         {
-            SetAnimFloat(Vector2.down);
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }
         }
-    }
 
     }
 
 
     private void ChangeState(EnemyState newState)
     {
-        if(currentState != newState)
+        if (currentState != newState)
         {
             currentState = newState;
         }
     }
-
-
+}
